20b2b5cdf8e1fa2d5baef018339beca72e2d8432
Shengcheechee <a0926072616@gmail.com>
2020-06-08 13:38:55 +0800

    Parse osm file and draw image
    1. Add drawing shortest path function.

--- parse_osm.py
+++ parse_osm.py
@@ -3,15 +3,14 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import cv2
 
-def parse_osm(filename):
+def parser(filename):
 
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    node_info = {}
+    nodes = {}
 
     for node in root.findall('node'):
-
         lat = float(node.get('lat'))
         lon = float(node.get('lon'))
         lat = int(lat * 100000 + 0.5)
@@ -22,12 +21,16 @@ def parse_osm(filename):
             if node.find('tag').get('v') == "traffic_signals":
                 trafsig = True
 
-        node_info[node.get('id')] = (lon, lat, trafsig)
+        node_info = {
+            'position': (lon, lat),
+            'trafsig': trafsig,
+            'belonged_ways': set()
+        }
+        nodes[node.get('id')] = node_info
 
-    ways = []
+    ways = {}
 
     for way in root.findall('way'):
-
         way_mem = []
         lane = 1
         oneway = 0
@@ -37,6 +40,7 @@ def parse_osm(filename):
             if (tag.get('k') == "highway" and tag.get('v') not in {"path", "footway", "steps", "pedestrian"}):
                 for nd in way.findall('nd'):
                     way_mem.append(nd.get('ref'))
+                    nodes[nd.get('ref')]['belonged_ways'].add(way.get('id'))
 
             if tag.get('k') == "lanes":
                 lane = int(tag.get('v'))
@@ -48,16 +52,20 @@ def parse_osm(filename):
                     oneway = -1
 
         if way_mem:
-            ways.append((way_mem, lane, oneway))
+            ways[way.get('id')] = (way_mem, lane, oneway)
 
-    lon_min_id = min(node_info.keys(), key = lambda nid: node_info[nid][0])
-    lon_max_id = max(node_info.keys(), key = lambda nid: node_info[nid][0])
-    lat_min_id = min(node_info.keys(), key = lambda nid: node_info[nid][1])
-    lat_max_id = max(node_info.keys(), key = lambda nid: node_info[nid][1])
-    x_min = node_info[lon_min_id][0]
-    x_max = node_info[lon_max_id][0]
-    y_min = node_info[lat_min_id][1]
-    y_max = node_info[lat_max_id][1]
+    return nodes, ways
+
+def render(nodes, ways, sp, path):
+
+    lon_min_id = min(nodes.keys(), key = lambda nid: nodes[nid]['position'][0])
+    lon_max_id = max(nodes.keys(), key = lambda nid: nodes[nid]['position'][0])
+    lat_min_id = min(nodes.keys(), key = lambda nid: nodes[nid]['position'][1])
+    lat_max_id = max(nodes.keys(), key = lambda nid: nodes[nid]['position'][1])
+    x_min = nodes[lon_min_id]['position'][0]
+    x_max = nodes[lon_max_id]['position'][0]
+    y_min = nodes[lat_min_id]['position'][1]
+    y_max = nodes[lat_max_id]['position'][1]
 
     width = x_max - x_min + 40
     height = y_max - y_min + 40
@@ -65,32 +73,58 @@ def parse_osm(filename):
     img = np.zeros((height, width, 3), np.uint16)
     img.fill(250)
 
-    for way in ways:
+    for way in ways.keys():
 
-        nd_temp = ""
+        nd_tmp = ""
+        start = False
 
-        for nd in way[0]:
+        for nd in ways[way][0]:
 
-            if nd_temp == "":
-                nd_temp = nd
+            if nd_tmp == "":
+                nd_tmp = nd
 
             else:
-                if way[2] == 1:
-                    cv2.line(img, (node_info[nd_temp][0] - x_min, y_max - node_info[nd_temp][1]), (node_info[nd][0] - x_min, y_max - node_info[nd][1]), (200, 0, 0), way[1])
-                elif way[2] == -1:
-                    cv2.line(img, (node_info[nd_temp][0] - x_min, y_max - node_info[nd_temp][1]), (node_info[nd][0] - x_min, y_max - node_info[nd][1]), (0, 200, 0), way[1])
+                if ways[way][2] == 1:
+                    cv2.line(img, (nodes[nd_tmp]['position'][0] - x_min, y_max - nodes[nd_tmp]['position'][1]), (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), (200, 0, 0), ways[way][1])
+                elif ways[way][2] == -1:
+                    cv2.line(img, (nodes[nd_tmp]['position'][0] - x_min, y_max - nodes[nd_tmp]['position'][1]), (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), (0, 200, 0), ways[way][1])
                 else:
-                    cv2.line(img, (node_info[nd_temp][0] - x_min, y_max - node_info[nd_temp][1]), (node_info[nd][0] - x_min, y_max - node_info[nd][1]), (150, 150, 150), way[1])
-                nd_temp = nd
+                    cv2.line(img, (nodes[nd_tmp]['position'][0] - x_min, y_max - nodes[nd_tmp]['position'][1]), (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), (150, 150, 150), ways[way][1])
+                nd_tmp = nd
 
-            cv2.circle(img, (node_info[nd][0] - x_min, y_max - node_info[nd][1]), 2, (0, 0, 0), -1)
+            cv2.circle(img, (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), 2, (0, 0, 0), -1)
 
-            if node_info[nd][2] == True:
-                cv2.circle(img, (node_info[nd][0] - x_min, y_max - node_info[nd][1]), 4, (0, 0, 255), 2)
+            if nodes[nd]['trafsig'] == True:
+                cv2.circle(img, (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), 4, (0, 0, 255), 2)
 
-            # cv2.putText(img, nd, (node_info[nd][0] - x_min + 1, y_max - node_info[nd][1] + 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 128, 128), 1, cv2.LINE_AA)
+            if len(nodes[nd]['belonged_ways']) > 1:
+                if nd in sp:
+                    cv2.circle(img, (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), 6, (0, 200, 200), 2)
+                    cv2.putText(img, nd, (nodes[nd]['position'][0] - x_min + 1, y_max - nodes[nd]['position'][1] + 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 128, 128), 1, cv2.LINE_AA)
+                else:
+                    cv2.circle(img, (nodes[nd]['position'][0] - x_min, y_max - nodes[nd]['position'][1]), 6, (200, 200, 0), 2)
 
-    cv2.imwrite('My_image_3.jpg', img)
+
+    for i in range(len(path)):
+
+        shortest_path = []
+
+        if ways[path[i]][0].index(sp[i + 1]) > ways[path[i]][0].index(sp[i]):
+            shortest_path = ways[path[i]][0][ways[path[i]][0].index(sp[i]) : ways[path[i]][0].index(sp[i + 1]) + 1]
+        else:
+            shortest_path = ways[path[i]][0][ways[path[i]][0].index(sp[i + 1]) : ways[path[i]][0].index(sp[i]) + 1]
+
+        sh_pa_tmp = ""
+
+        for sh_pa in shortest_path:
+            if sh_pa_tmp == "":
+                sh_pa_tmp = sh_pa
+            else:
+                cv2.line(img, (nodes[sh_pa_tmp]['position'][0] - x_min, y_max - nodes[sh_pa_tmp]['position'][1]), (nodes[sh_pa]['position'][0] - x_min, y_max - nodes[sh_pa]['position'][1]), (0, 0, 200), ways[path[i]][1])
+                sh_pa_tmp = sh_pa
+
+    cv2.imwrite('My_map.jpg', img)
 
 if __name__ == '__main__':
-    parse_osm('nn.osm')
+    nodes, ways = parser('ncku.osm')
+    render(nodes, ways, [], [])
