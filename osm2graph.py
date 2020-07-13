ed533b2d88c82c1efe91024e90d01d70eacb2cc4
Shengcheechee <a0926072616@gmail.com>
2020-07-13 14:57:27 +0800

    Add node and edge attribute.

--- osm2graph.py
+++ osm2graph.py
@@ -3,37 +3,35 @@ import networkx as nx
 import random
 from parse_osm import parser, render
 
-def converter(nodes, ways):
-    graph_nodes = []
+def convert2graph(nodes, ways):
+    osm_graph = nx.DiGraph()
 
     for node in nodes.keys():
         if len(nodes[node]['belonged_ways']) > 1:
-            graph_nodes.append(node)
+            osm_graph.add_node(node, trafsig = nodes[node]['trafsig'])
 
-    osm_graph = nx.DiGraph()
-    osm_graph.add_nodes_from(graph_nodes)
     for way in ways.keys():
         nd_tmp = ""
         for nd in ways[way][0]:
-            if nd in graph_nodes:
+            if nd in list(osm_graph):
                 if ways[way][2] == 1:
                     if nd_tmp == "":
                         nd_tmp = nd
                     else:
-                        osm_graph.add_edge(nd_tmp, nd, id = way)
+                        osm_graph.add_edge(nd_tmp, nd, id = way, lane = ways[way][1], priority = ways[way][3])
                         nd_tmp = nd
                 elif ways[way][2] == -1:
                     if nd_tmp == "":
                         nd_tmp = nd
                     else:
-                        osm_graph.add_edge(nd, nd_tmp, id = way)
+                        osm_graph.add_edge(nd, nd_tmp, id = way, lane = ways[way][1], priority = ways[way][3])
                         nd_tmp = nd
                 else:
                     if nd_tmp == "":
                         nd_tmp = nd
                     else:
-                        osm_graph.add_edge(nd_tmp, nd, id = way)
-                        osm_graph.add_edge(nd, nd_tmp, id = way)
+                        osm_graph.add_edge(nd_tmp, nd, id = way, lane = ways[way][1], priority = ways[way][3])
+                        osm_graph.add_edge(nd, nd_tmp, id = way, lane = ways[way][1], priority = ways[way][3])
                         nd_tmp = nd
 
     return osm_graph
@@ -56,19 +54,19 @@ def shortest_path(nx_graph, s, t):
     return sp_nodes, path_ids, path_edges
 
 def get_subgraph(nx_graph, edge):
-    graph_1 = nx.ego_graph(nx_graph, edge[0], radius = 5)
-    graph_2 = nx.ego_graph(nx_graph, edge[1], radius = 5)
+    graph_1 = nx.ego_graph(nx_graph, edge[0], radius = 2)
+    graph_2 = nx.ego_graph(nx_graph, edge[1], radius = 2)
     subgraph = nx.compose(graph_1, graph_2)
 
     return subgraph
 
 if __name__ == '__main__':
     nodes, ways = parser('ncku.osm')
-    nx_graph = converter(nodes, ways)
+    nx_graph = convert2graph(nodes, ways)
     source = random.choice(list(nx_graph))
     target = random.choice(list(nx_graph))
     sp_nodes, path_ids, path_edges = shortest_path(nx_graph, source, target)
     subgraphs = []
     for edge in path_edges:
         subgraphs.append(get_subgraph(nx_graph, edge))
-    render(nodes, ways, sp_nodes, path_ids)
+    x_min, y_max = render(nodes, ways, sp_nodes, path_ids, subgraphs)
