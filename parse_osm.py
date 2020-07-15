b3585e90f15bbcabc5c8a710ff7d998e6840f1ff
Shengcheechee <a0926072616@gmail.com>
2020-07-15 22:42:07 +0800

    Modif someting
    1. Change the way to save "oneway".
    2. Change the data type of "ways".
    3. Add "affine" function to simplify code.
    4. Add the function to draw subgraphs on map.

--- parse_osm.py
+++ parse_osm.py
@@ -27,12 +27,20 @@ def parser(filename):
     for way in root.findall('way'):
         way_mem = []
         lane = 1
-        oneway = 0
         priority = ""
+        way_name = ""
 
         for tag in way.findall('tag'):
             if (tag.get('k') == "highway" and tag.get('v') not in {"path", "footway", "steps", "pedestrian"}):
-                priority = tag.get('v')
+                if tag.get('v') == "primary":
+                    priority = 10
+                elif tag.get('v') == "secondary":
+                    priority = 8
+                elif tag.get('v') == "tertiary":
+                    priority = 6
+                else:
+                    priority = 1
+
                 for nd in way.findall('nd'):
                     way_mem.append(nd.get('ref'))
                     nodes[nd.get('ref')]['belonged_ways'].add(way.get('id'))
@@ -40,16 +48,20 @@ def parser(filename):
             if tag.get('k') == "lanes":
                 lane = int(tag.get('v'))
 
-            #! why not just inverse way_member
             if tag.get('k') == "oneway":
-                if tag.get('v') == "yes":
-                    oneway = 1
+                oneway =  tag.get('v') == "yes" or tag.get('v') == "-1"
                 if tag.get('v') == "-1":
-                    oneway = -1
+                    way_mem.reverse()
+            else:
+                oneway = False
 
         if way_mem:
-            #! Use dict
-            ways[way.get('id')] = (way_mem, lane, oneway, priority)
+            ways[way.get('id')] = {
+                'way_member': way_mem,
+                'lane': lane,
+                'oneway': oneway,
+                'priority': priority
+            }
 
     return nodes, ways
 
@@ -58,13 +70,7 @@ def gen_affine(x_min, y_max):
         return (node['position'][0] - x_min, y_max - node['position'][1])
     return affine
 
-#! variable name sp and path should be more clear. It is really hard to tell
-#! meaning from variable name.
-# shortest_path = {
-#     "nodes": [...],
-#     "edges": [...],
-# }
-def render(nodes, ways, sp, path):
+def render(nodes, ways, sp_nodes, path_ids, subgraphs):
     lon_min_id = min(nodes.keys(), key = lambda nid: nodes[nid]['position'][0])
     lon_max_id = max(nodes.keys(), key = lambda nid: nodes[nid]['position'][0])
     lat_min_id = min(nodes.keys(), key = lambda nid: nodes[nid]['position'][1])
@@ -73,6 +79,8 @@ def render(nodes, ways, sp, path):
     x_max = nodes[lon_max_id]['position'][0]
     y_min = nodes[lat_min_id]['position'][1]
     y_max = nodes[lat_max_id]['position'][1]
+
+    affine = gen_affine(x_min, y_max)
 
     width = x_max - x_min + 40
     height = y_max - y_min + 40
@@ -84,48 +92,45 @@ def render(nodes, ways, sp, path):
         nd_tmp = ""
         start = False
 
-        for nd in ways[way][0]:
+        for nd in ways[way]['way_member']:
             if nd_tmp == "":
                 nd_tmp = nd
 
             else:
-                if ways[way][2] == 1:
-                    cv2.line(img, affine(nodes[nd_tmp]), affine(nodes[nd]), (200,   0,   0), ways[way][1])
-                elif ways[way][2] == -1:
-                    cv2.line(img, affine(nodes[nd_tmp]), affine(nodes[nd]), (  0, 200,   0), ways[way][1])
+                if ways[way]['oneway']:
+                    cv2.line(img, affine(nodes[nd_tmp]), affine(nodes[nd]), (200,   0,   0), ways[way]['lane'])
                 else:
-                    cv2.line(img, affine(nodes[nd_tmp]), affine(nodes[nd]), (150, 150, 150), ways[way][1])
+                    cv2.line(img, affine(nodes[nd_tmp]), affine(nodes[nd]), (150, 150, 150), ways[way]['lane'])
                 nd_tmp = nd
 
             cv2.circle(img, affine(nodes[nd]), 2, (0, 0, 0), -1)
 
-            if nodes[nd]['trafsig'] == True:
+            if nodes[nd]['trafsig']:
                 cv2.circle(img, affine(nodes[nd]), 4, (0, 0, 255), 2)
 
             if len(nodes[nd]['belonged_ways']) > 1:
-                if nd in sp:
+                if nd in sp_nodes:
                     cv2.circle(img, affine(nodes[nd]), 6, (0, 200, 200), 2)
                     cv2.putText(img, nd, (nodes[nd]['position'][0] - x_min + 1, y_max - nodes[nd]['position'][1] + 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 128, 128), 1, cv2.LINE_AA)
+
                 else:
                     cv2.circle(img, affine(nodes[nd]), 6, (200, 200, 0), 2)
 
+    for i in range(len(path_ids)):
+        shortest_path_ids = ways[path_ids[i]]['way_member'][ways[path_ids[i]]['way_member'].index(sp_nodes[i]) : ways[path_ids[i]]['way_member'].index(sp_nodes[i + 1]) + 1]
+        for j in range(len(shortest_path_ids) - 1):
+            cv2.line(img, affine(nodes[shortest_path_ids[j]]), affine(nodes[shortest_path_ids[j + 1]]), (0, 0, 200), ways[path_ids[i]]['lane'])
 
-    for i in range(len(path)):
-        shortest_path = []
+    count = 0
+    for subgraph in subgraphs:
+        for sg_nd in list(subgraph):
+            cv2.circle(img, (nodes[sg_nd]['position'][0] - x_min, y_max - nodes[sg_nd]['position'][1]), 10, (200, 0, 200), 2)
+            cv2.putText(img, sg_nd, (nodes[sg_nd]['position'][0] - x_min + 1, y_max - nodes[sg_nd]['position'][1] + 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 128, 128), 1, cv2.LINE_AA)
+        cv2.imwrite('My_map_%d.jpg'%(count), img)
+        count += 1
 
-        #! the if-else here is unnecessary after inversion.
-        if ways[path[i]][0].index(sp[i + 1]) > ways[path[i]][0].index(sp[i]):
-            shortest_path = ways[path[i]][0][ways[path[i]][0].index(sp[i]) : ways[path[i]][0].index(sp[i + 1]) + 1]
-        else:
-            shortest_path = ways[path[i]][0][ways[path[i]][0].index(sp[i + 1]) : ways[path[i]][0].index(sp[i]) + 1]
-
-        sh_pa_tmp = ""
-
-        for j in range(len(shortest_path) - 1):
-            cv2.line(img, affine(nodes[shortest_path[j]]), affine(nodes[shortest_path[j+1]]), (0, 0, 200), ways[path[i]][1])
-
-    cv2.imwrite('My_map.jpg', img)
+    return x_min, y_max
 
 if __name__ == '__main__':
     nodes, ways = parser('ncku.osm')
-    render(nodes, ways, [], [])
+    render(nodes, ways, [], [], [])
